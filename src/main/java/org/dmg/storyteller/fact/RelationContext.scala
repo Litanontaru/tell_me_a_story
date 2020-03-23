eea4ade939275284e6942d9b6b32c5f62750e4f5
litanontaru <Ghtvmthf5%t>
2020-03-23 17:51:32 +0100

    relation can be context like

--- src/main/java/org/dmg/storyteller/fact/RelationContext.scala
+++ src/main/java/org/dmg/storyteller/fact/RelationContext.scala
@@ -1,6 +1,6 @@
 package org.dmg.storyteller.fact
 
-class RelationContext(val link: Context, relation: Context) extends RelationContextLike(link, relation) with Context {
+class RelationContext(val link: Context, relation: ContextLike) extends RelationContextLike(link, relation) with Context {
   override def :+(fact: Fact): Context = link :+ fact
 
   override def :-(fact: Fact): Context = link :- fact

edbce12833b4ae32061b55abfd7ac6fe3c2ab7ad
litanontaru <Ghtvmthf5%t>
2020-03-23 21:04:56 +0100

    removed redundant val

--- src/main/java/org/dmg/storyteller/fact/ViewContext.scala
+++ src/main/java/org/dmg/storyteller/fact/ViewContext.scala
@@ -1,6 +1,6 @@
 package org.dmg.storyteller.fact
 
-class ViewContext(base: ContextLike, val view: Context) extends ViewContextLike(base, view) with Context {
+class ViewContext(base: ContextLike, view: Context) extends ViewContextLike(base, view) with Context {
   override def :+(fact: Fact): Context = view :+ fact
 
   override def :-(fact: Fact): Context = view :- fact
